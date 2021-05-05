85ebe90bf9dfca973cef807b9b4ac4b0829d3e32
Matheus Rubens <matheusrb8@outlook.com.br>
2021-05-05 16:45:12 -0300

    Criando arquivo de teste do comando Stash

---
+++ testandoStash.py
@@ -0,0 +1,9 @@
+print("Testando comando (git stash).")
+
+print("Bem-vindo ao meu programa de somar dois valores!\n")
+
+valor1 = int(input("Digite o primeiro valor: "))
+valor2 = int(input("Agora, digite o segundo valor: "))
+valorFinal = valor1 + valor2
+
+print("O resultado da soma entre {0} e {1} é {2}.".format(valor1,valor2,valorFinal))
